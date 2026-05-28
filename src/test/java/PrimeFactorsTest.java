af8025defd9056a06c7572c8880900123b3ef8e8
yongwoo <yongwoo8.kim@samsung.com>
2026-05-28 09:40:17 +0900

    [feature] 34

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -10,4 +10,10 @@ class PrimeFactorsTest {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(Arrays.asList(), primeFactor.of(1));
     }
+
+    @Test
+    void testPrimeFactorOf02() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(2), primeFactor.of(2));
+    }
 }
