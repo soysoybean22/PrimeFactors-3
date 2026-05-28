4006c95d1704b76675671082d4fe4ed69501ffed
yongwoo <yongwoo8.kim@samsung.com>
2026-05-28 09:38:14 +0900

    [feature] 29

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorsTest {
     @Test
     void testPrimeFactorOf01() {
         PrimeFactor primeFactor = new PrimeFactor();
-        assertEquals(10, 10);
+        assertEquals(Arrays.asList(), primeFactor.of(1));
     }
 }
