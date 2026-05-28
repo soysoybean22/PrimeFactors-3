241aa8170297c146743774d26f8563ff26501ec4
yongwoo <yongwoo8.kim@samsung.com>
2026-05-28 09:45:56 +0900

    [refactor] 42

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -5,12 +5,8 @@ public class PrimeFactor {
     public Object of(int number) {
         List<Integer> factors = new ArrayList<>();
 
-        if (number == 2) {
-            factors.add(2);
-        }
-
-        if (number == 3) {
-            factors.add(3);
+        if (number > 1) {
+            factors.add(number);
         }
 
         return factors;
